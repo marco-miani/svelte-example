fb793b07ba4dbd70185fbd2e2af3af62a6147dc5
marco-miani <mmiani21@gmail.com>
2023-06-04 18:44:36 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../app.css";
+    import Header from "$lib/Header.svelte";
+</script>
+
+<style>
+    @font-face {
+        font-family: 'Roboto', sans-serif;
+        src: url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
+    }
+
+    * {
+        font-family: 'Roboto', sans-serif;
+    }
+</style>
+
+<Header />
+
+
+
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Body from "$lib/Body.svelte";
+    import Footer from "$lib/Footer.svelte";
+</script>
+
+
+<Body />
+<Footer />
+
+<slot />
